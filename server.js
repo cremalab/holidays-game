d3faa86ee232be9614a9231d6441dd8fc50cb296
Ross Brown <ross@cremalab.com>
2015-12-23 17:41:05 -0600

    adding procfile and server

---
+++ server.js
@@ -0,0 +1,20 @@
+var http        = require('http');
+var path        = require('path');
+var express     = require('express');
+var compression = require('compression');
+var request     = require('request');
+var httpProxy   = require('node-http-proxy');
+var app         = express();
+var env         = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+app.use(compression())
+app.use(express.static(process.cwd() + '/public'));
+
+app.get('/*', function(req, res){
+  var htmlPath = path.join(process.cwd(), 'public/index.html');
+  res.sendFile(htmlPath);
+});
+var port = Number(process.env.PORT || 5000);
+var server = http.createServer(app).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
